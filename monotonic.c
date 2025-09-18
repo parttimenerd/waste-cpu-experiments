22628d2afe5d9682fc5b42a5a6ea865c142c574f
Johannes Bechberger <johannes.bechberger@sap.com>
2025-09-18 22:30:21 +0200

    Add monotonic

---
+++ monotonic.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <bits/time.h>
+#include <time.h>
+
+long own_clock_nanos() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1e9 + ts.tv_nsec;
+}
+
+void wait(int seconds) {
+    long end_time = own_clock_nanos() + seconds * 1e9;
+    while (own_clock_nanos() < end_time) {
+        // cpu wasting loop
+    }
+}
